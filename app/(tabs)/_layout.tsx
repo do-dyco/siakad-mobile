add5269c0cf118d40dd707c84641db4b32d0756a
do-dyco <dodhydnkz24@gmail.com>
2024-11-03 16:24:17 +0700

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,11 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Ionicons, Octicons, SimpleLineIcons } from "@expo/vector-icons";
+import colors from "@/src/config/colors";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -11,24 +13,62 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <Octicons
+              name={"home"}
+              color={focused ? colors.primary : color}
+              size={25}
+            />
           ),
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="akademik"
         options={{
-          title: 'Explore',
+          title: "Akademik",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <SimpleLineIcons
+              name={"badge"}
+              color={focused ? colors.primary : color}
+              size={25}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="keuangan"
+        options={{
+          title: "Keuangan",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={"wallet-outline"}
+              color={focused ? colors.primary : color}
+              size={25}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="akun"
+        options={{
+          title: "Akun",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={"person-circle-outline"}
+              color={focused ? colors.primary : color}
+              size={32}
+            />
           ),
         }}
       />
